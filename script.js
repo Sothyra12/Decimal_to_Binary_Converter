703c7ff399768fd9ed88e219af8b51493ea66e35
Sothyra12 <schan256@my.centennialcollege.ca>
2024-10-28 23:29:46 -0400

    get element by id, create a function to test the input value, and add a click event listener

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 /* script.js */
+
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+// test or check the input value when the convert button is clicked
+const checkUserInput = () => {
+  console.log(numberInput.value);
+};
+
+// the click event takes a reference to checkUserInput function - reference is not called with ()
+convertBtn.addEventListener("click", checkUserInput);
